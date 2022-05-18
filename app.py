e0b23957cfb0daf075705b064c3cb2378c1f0147
Daegeun Jeong <wjdeorms27@naver.com>
2022-05-19 00:39:51 +0900

    Feat: add authorize function & article(post & get)

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-import datetime
+from datetime import datetime, timedelta
+from functools import wraps
 from bson import ObjectId
 from pymongo import MongoClient
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 import hashlib
 import json
 from flask_cors import CORS
-
 import jwt
+
+# decorator 함수를 사용해 사용자의 토큰값 확인
+
+
+def authorize(f):
+    @wraps(f)
+    def decorated_function():
+        # 만약 Authorization이 헤더 안에 없다면
+        if not 'Authorization' in request.headers:
+            abort(401)  # 401 에러를 반환
+        # Authorization이 헤더 안에 있다면 'token' 변수에 저장
+        token = request.headers['Authorization']
+        try:
+            # 토큰을 디코드한 값을 user에 저장
+            # _id, email, exp가 들어있음
+            user = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        except:
+            abort(401)
+        return f(user)
+    return decorated_function
 
 
 app = Flask(__name__)
@@ -19,7 +39,9 @@ SECRET_KEY = 'turtle'
 
 
 @app.route("/")
-def home():
+@authorize
+def home(user):
+    print(user)
     return jsonify({'msg': 'success'})
 
 
@@ -73,7 +95,7 @@ def sign_in():
         payload = {
             'id': str(my_signin['_id']),
             'email': my_email,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60 * 60 * 24)
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
         }
         print(payload)
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
@@ -85,17 +107,51 @@ def sign_in():
 
 
 @app.route("/getuserinfo", methods=['GET'])
-def get_user_info():
-    token = request.headers.get('Authorization')
-    if not token:
-        return jsonify({'msg': 'check your token again...'})
-    print('1', token)
-    user_payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-    print('2', user_payload)
-    user_info = db.user.find_one({'_id': ObjectId(user_payload['id'])})
+@authorize
+def get_user_info(user):
+
+    user_info = db.user.find_one({'_id': ObjectId(user['id'])})
     print('3', user_info)
 
     return jsonify({'msg': 'success', 'email': user_info['email']})
+
+
+@app.route("/article", methods=['POST'])
+@authorize
+def post_article(user):
+    data = json.loads(request.data)
+    print(data)
+
+    # 여기서의 user는 authorize에 들어가는 인자값
+    # user에는 payload로 만들어진 token이 존재하고, payload에는 사용자의 str화 시킨 _id가 존재한다.
+    # 이 값을 mongoDB에서 사용하기 위해서는 다시 ObjectId로 변환해줘야 하기 때문에 아래와 같은 작업 진행
+    db_user = db.user.find_one({'_id': ObjectId(user.get('id'))})
+
+    # 현재 시각 표시
+    now_date = datetime.datetime.now().strftime('%H : %M : %S')
+
+    doc = {
+        'title': data.get('title', None),  # 현재는 None 처리를 통해 값이 없더라도 입력 가능
+        # 추후 값이 없을 때 에러 처리하는 것을 만들어 활용해보기
+        'content': data.get('content', None),
+        'user': user['id'],
+        'user_email': db_user['email'],
+        'time': now_date
+    }
+    print(doc)
+    db.article.insert_one(doc)
+    return jsonify({'msg': 'success'})
+
+
+@app.route('/article', methods=['GET'])
+def get_article():
+    articles = list(db.article.find())
+
+    # articles의 모든 _id의 값을 str화해야 json 형식으로 보낼 수 있기 때문에 해당 작업 진행
+    for article in articles:
+        article['_id'] = str(article['_id'])
+
+    return jsonify({'msg': 'success', 'articles': articles})
 
 
 if __name__ == '__main__':
